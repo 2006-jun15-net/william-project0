4a2305ebf965a2a06e2c5f54fd769c044d430b70
WNesham <William.Nesham@gmail.com>
2020-06-19 15:51:07 -0500

    Initial commit and display welcome message.

---
+++ ProjectZero/ProjectZero/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi! Welcome to Willy's!");
+        }
+    }
+}
